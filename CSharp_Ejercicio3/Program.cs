b405800f32213d83d6a072aa353f6734f36851e1
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 17:41:27 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Ejercicio3/Program.cs
@@ -0,0 +1,15 @@
+﻿////Pedir el nombre de la semana al usuario y decirle si es fin de semana o no. En caso de error, indicarlo
+Console.WriteLine("Ingrese el nombre de un día de la semana:");
+string dia = Console.ReadLine().ToLower();
+if (dia == "sabado" || dia == "domingo")
+{
+    Console.WriteLine("Es fin de semana.");
+}
+else if (dia == "lunes" || dia == "martes" || dia == "miercoles" || dia == "jueves" || dia == "viernes")
+{
+    Console.WriteLine("No es fin de semana.");
+}
+else
+{
+    Console.WriteLine("El día ingresado no es válido.");
+}
